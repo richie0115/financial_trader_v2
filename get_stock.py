089e748f32e5959176af003e0c9326ce3d559644
richie0115 <richie920115@gmail.com>
2025-10-15 23:31:10 +0800

    add download csv function

--- get_stock.py
+++ get_stock.py
@@ -3,6 +3,8 @@
 ################################
 
 import yfinance as yf
+import datetime
+import numpy as np
 
 def get_stock_info(ticker_symbol):
     """
@@ -29,11 +31,40 @@ def get_stock_info(ticker_symbol):
     except Exception as e:
         print(f"An error occurred: {e}")
         return None 
-    
 
+def get_historical_data(ticker_symbol, period="5y", interval="1d"):
+    """
+    Fetches historical stock data for a given ticker symbol.
+
+    """
+    try:
+        stock = yf.Ticker(ticker_symbol)
+        hist = stock.history(period=period, interval=interval)
+        return hist
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None 
+
+def download_stock_data(ticker_symbol, filename):
+    """
+    Downloads historical stock data and saves it to a CSV file.
+
+    """
+    today = datetime.date.today()
+    data = yf.download([ticker_symbol], start="2020-01-01", end=today)
+
+    if data is not None:
+        data.to_csv(filename)
+        print(f"Historical data for {ticker_symbol} saved to {filename}")
+    else:
+        print("Failed to download or save historical data.")
+        
 if __name__ == "__main__":
     ticker = "AAPL"
     stock_data = get_stock_info(ticker)
+    historical_data = get_historical_data(ticker)
+    data = download_stock_data(ticker, "AAPL_historical_data.csv")
     if stock_data:
         print(f"Stock information for {ticker}:")
         for key, value in stock_data.items():
